5b8bb94c4b3b270a3e0572ce80961d93a28f0036
Bart Broere <mail@bartbroere.eu>
2021-10-27 20:27:50 +0200

    Add master push to pypi (#13)

--- setup.py
+++ setup.py
@@ -13,6 +13,9 @@ dependencies = [
     'tqdm',
 ]
 
+with open('README.md', 'r') as r:
+    long_description = r.read()
+
 setup(
     name='demeuk',
     version=version,
@@ -21,4 +24,16 @@ setup(
     description='CLI tool to remove invalid chars from a corpus.',
     install_requires=dependencies,
     scripts=['bin/demeuk.py'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/NetherlandsForensicInstitute/demeuk",
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
 )
