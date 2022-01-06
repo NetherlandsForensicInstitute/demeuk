50e1d369d203aa0567270440c4ba4a5d421213f3
Rick van der Zwet <r.van.der.zwet@nfi.nl>
2022-01-06 17:15:44 +0100

    Bump version newline feature will change output

--- bin/demeuk.py
+++ bin/demeuk.py
@@ -116,7 +116,7 @@ from tqdm import tqdm
 from unidecode import unidecode
 
 
-version = '3.9.4'
+version = '3.9.5'
 
 HEX_REGEX = re_compile(r'\$HEX\[([0-9a-f]+)\]')
 EMAIL_REGEX = '.{1,64}@([a-zA-Z0-9_-]{1,63}\\.){1,3}[a-zA-Z]{2,6}'
