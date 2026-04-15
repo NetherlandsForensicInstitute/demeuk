e38e22306495a0cdbeda6c87ca2aa12691a11bc9
Melvin Frederiks <m.frederiks@nfi.nl>
2026-04-15 10:52:05 +0200

    Fixed hash regex

--- modules/regexes.py
+++ modules/regexes.py
@@ -18,8 +18,8 @@ HASH_BCRYPT_REGEX = '^\\$1[ayb]\\$[0-9]{1,}\\$[\\w\\.\\/]{4,}$'
 # $0$a-zA-Z0-9./ min length 12 to make sure we hit somthing like: a-zA-Z0-9./
 # this will cause string like $0$JAjdna./d to still be included.
 
-HASH_CRYPT_REGEX = '^\\$[1355]\\$[\\w\\.\\/]{12,}$'
-HASH_CRYPT_SALT_REGEX = '^\\$[1355]\\$[\\w\\.\\/\\+]{,16}\\$[\\w\\.\\/]{6,}$'
+HASH_CRYPT_REGEX = '^\\$[1356]\\$[\\w\\.\\/]{12,}$'
+HASH_CRYPT_SALT_REGEX = '^\\$[1356]\\$[\\w\\.\\/\\+]{,16}\\$[\\w\\.\\/]{6,}$'
 HASH_PHPBB_REGEX = '^\\$[hH]\\$[\\w\\.\\/]{5,}$'
 HASH_REGEX_LIST = [HASH_BCRYPT_REGEX, HASH_CRYPT_SALT_REGEX, HASH_CRYPT_REGEX, HASH_PHPBB_REGEX]
 
