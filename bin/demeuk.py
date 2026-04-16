23b992d2876843d344fa37b82c54d0eb416383ff
Melvin Frederiks <m.frederiks@nfi.nl>
2026-04-16 16:21:41 +0200

    Bump version

--- bin/demeuk.py
+++ bin/demeuk.py
@@ -180,7 +180,7 @@ from transliterate import translit
 from unidecode import unidecode
 
 
-version = '4.6.2'
+version = '4.6.3'
 
 # Search from start to finish for the string $HEX[], with block of a-f0-9 with even number
 # of hex chars. The first match group is repeated.
