d3faaaedf7bf4383164f217045ef530df84a61c8
Melvin Frederiks <m.frederiks@nfi.nl>
2026-05-01 15:45:24 +0200

    Add bounds-check modules

---
+++ src/demeuk/modules/check/bounds.py
@@ -0,0 +1,88 @@
+from .check import CheckModule
+from ..base import *
+
+# Maybe add a BoundsCheckModule which automatically creates min/max versions?
+
+class MinDigitsModule(CheckModule, ParamModule):
+    @staticmethod
+    def get_help_info():
+        return HelpInfoParam(
+            option='check-min-digits',
+            help_str='Require that entries contain at least <N> digits.',
+            metavar='<N>',
+            param_type=int)
+
+    def run(self, line):
+        if sum([c.isdigit() for c in line]) < self.param:
+            return self.stop
+        return self.next
+
+class MaxDigitsModule(CheckModule, ParamModule):
+    @staticmethod
+    def get_help_info():
+        return HelpInfoParam(
+            option='check-max-digits',
+            help_str='Require that entries contain at most <N> digits.',
+            metavar='<N>',
+            param_type=int)
+
+    def run(self, line):
+        if sum([c.isdigit() for c in line]) > self.param:
+            return self.stop
+        return self.next
+
+class MinUppercaseModule(CheckModule, ParamModule):
+    @staticmethod
+    def get_help_info():
+        return HelpInfoParam(
+            option='check-min-uppercase',
+            help_str='Require that entries contain at least <N> uppercase characters.',
+            metavar='<N>',
+            param_type=int)
+
+    def run(self, line):
+        if sum([c.isupper() for c in line]) < self.param:
+            return self.stop
+        return self.next
+
+class MaxUppercaseModule(CheckModule, ParamModule):
+    @staticmethod
+    def get_help_info():
+        return HelpInfoParam(
+            option='check-max-uppercase',
+            help_str='Require that entries contain at most <N> uppercase characters.',
+            metavar='<N>',
+            param_type=int)
+
+    def run(self, line):
+        if sum([c.isupper() for c in line]) > self.param:
+            return self.stop
+        return self.next
+
+class MinSpecialsModule(CheckModule, ParamModule):
+    @staticmethod
+    def get_help_info():
+        return HelpInfoParam(
+            option='check-min-specials',
+            help_str='Require that entries contain at least <N> special characters.',
+            metavar='<N>',
+            param_type=int)
+
+    def run(self, line):
+        if sum([not c.isanum() and not c.isspace() for c in line]) < self.param:
+            return self.stop
+        return self.next
+
+class MaxSpecialsModule(CheckModule, ParamModule):
+    @staticmethod
+    def get_help_info():
+        return HelpInfoParam(
+            option='check-max-specials',
+            help_str='Require that entries contain at most <N> special characters.',
+            metavar='<N>',
+            param_type=int)
+
+    def run(self, line):
+        if sum([not c.isanum() and not c.isspace() for c in line]) > self.param:
+            return self.stop
+        return self.next
