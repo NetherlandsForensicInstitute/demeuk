c9d2c42d4d3f041831372481843712d8537dbe7a
Melvin Frederiks <m.frederiks@nfi.nl>
2026-05-01 10:48:27 +0200

    Ported --googlengram

--- src/demeuk/parser2.py
+++ src/demeuk/parser2.py
@@ -2,7 +2,11 @@ from argparse import ArgumentTypeError, ArgumentParser, RawDescriptionHelpFormat
 from os import cpu_count
 from textwrap import dedent
 
+from demeuk.modules.add import AddModule
 from demeuk.modules.base import ParamModule
+from demeuk.modules.check import CheckModule
+from demeuk.modules.macro import MacroModule
+from demeuk.modules.modify import ModifyModule
 
 
 # -j can take int or 'all' as argument.
