32a39737587eab0c73e5b34cac6cc50f3dc38714
Melvin Frederiks <m.frederiks@nfi.nl>
2026-05-01 13:54:53 +0200

    Moved leak module

---
+++ src/demeuk/modules/macro/leak.py
@@ -0,0 +1,63 @@
+from .macro import MacroModule
+from ..base import *
+
+from ..modify.encode import EncodeModule
+from ..modify.html import *
+from ..modify.modify import MojibakeModule, NewlineModule
+from ..modify.hex import HexModule
+from ..check.check import ControlCharModule
+
+# NB: Macro module contains config module as submodule. So we pass the config on...
+# Maybe not the best way? or not too bad...
+class LeakModule(MacroModule, ConfigModule):
+    def set_configs(self, config):
+        # Store the entire config as config...
+        self.add_config('config', config)
+
+    @staticmethod
+    def get_help_info():
+        return HelpInfo(
+            option='leak',
+            help_str='When set, demeuk will run the following modules: mojibake, encode, newline, check-controlchar. '
+                     'This is recommended when working with leaks.'
+        )
+
+    def get_submodules(self):
+        # We need to instantiate this explicitly because we want to pass config.
+        encode_module = EncodeModule()
+        encode_module.set_configs(self.get_config('config'))
+        return [
+            encode_module,
+            MojibakeModule(),
+            ControlCharModule(),
+            NewlineModule(),
+        ]
+
+class LeakFullModule(MacroModule, ConfigModule):
+    def set_configs(self, config):
+        self.add_config('config', config)
+
+    @staticmethod
+    def get_help_info():
+        return HelpInfo(
+            option='leak-full',
+            help_str='When set, demeuk will run the following modules: mojibake, encode, newline, check-controlchar, '
+                     'hex, html, html-named, check-hash, check-mac-address, check-uuid, check-email, '
+                     'check-replacement-character, check-empty,line.'
+        )
+
+    def get_submodules(self):
+        encode_module = EncodeModule()
+        encode_module.set_configs(self.get_config('config'))
+        return [
+            encode_module,
+            MojibakeModule(),
+            ControlCharModule(),
+            NewlineModule(),
+            HexModule(),
+            HtmlModule(),
+            HtmlNamedModule(),
+        ]
+
+
+
--- src/demeuk/modules/macro/macro.py
+++ src/demeuk/modules/macro/macro.py
@@ -34,30 +34,6 @@ class MacroModule(Module):
         return PipelinePosition.AFTER_ENCODE
 
 
-# NB: Macro module contains config module as submodule. So we pass the config on...
-# Maybe not the best way? or not too bad...
-class LeakModule(MacroModule, ConfigModule):
-    def set_configs(self, config):
-        # Store the entire config as config...
-        self.add_config('config', config)
-
-    @staticmethod
-    def get_help_info():
-        return HelpInfo(
-            option='leak',
-            help_str='When set, demeuk will run the following modules: mojibake, encode, newline, check-controlchar. This is recommended when working with leaks.'
-        )
-
-    def get_submodules(self):
-        # We need to instantiate this explicitly because we want to pass config.
-        encode_module = EncodeModule()
-        encode_module.set_configs(self.get_config('config'))
-        return [
-            MojibakeModule(),
-            encode_module,
-            NewlineModule(),
-            ControlCharModule(),
-        ]
 
 class GoogleNgramModule(MacroModule, ConfigModule):
 
