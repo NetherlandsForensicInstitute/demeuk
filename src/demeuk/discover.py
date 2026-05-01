5e4ab62107fea347933ebce21cf22a50c60f2a63
Melvin Frederiks <m.frederiks@nfi.nl>
2026-05-01 12:52:33 +0200

    Module discovery now looks recursively inside subdirectories

--- src/demeuk/discover.py
+++ src/demeuk/discover.py
@@ -5,9 +5,11 @@ import os
 
 # Discover modules in demeuk/modules
 # TODO discover at custom location maybe? Check if this is possible
+# TODO look at pathlib for this
 def discover_modules():
     project_dir = os.path.dirname(__file__)
     modules_dir = project_dir + '/modules/'
+
 
     classes = set()
 
@@ -19,14 +21,18 @@ def discover_modules():
                  'WhitespaceTokenizer', # Not sure why this one is included...
                  ]
 
-    for file in os.listdir(modules_dir):
-        # Look for non-hidden python scripts
-        if file.endswith('.py') and not file.startswith('_'):
-            # Truncate file extension
-            module_name = 'demeuk.modules.' + file[:-3]
-            members = inspect.getmembers(importlib.import_module(module_name))
-            for name, obj in members:
-                if inspect.isclass(obj) and name not in blacklist:
-                    classes |= {obj}
+    # Recursively look through subdirectories of /modules
+    for path, names, files in os.walk(modules_dir):
+        for file in files:
+            # Look for non-hidden python scripts
+            if file.endswith('.py') and not file.startswith('_'):
+                relative_path = os.path.relpath(os.path.join(path, file), modules_dir)
+                # Truncate file extension
+                module_name = 'demeuk.modules.' + relative_path[:-3].replace('/', '.')
+                print(f'import {module_name}')
+                members = inspect.getmembers(importlib.import_module(module_name))
+                for name, obj in members:
+                    if inspect.isclass(obj) and name not in blacklist:
+                        classes |= {obj}
 
     return classes
