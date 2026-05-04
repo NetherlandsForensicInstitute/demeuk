cf7a291669d24d89779bddf5ce3465f0be74077a
Melvin Frederiks <m.frederiks@nfi.nl>
2026-05-04 11:43:30 +0200

    Fixed test_verbose, new logging messages

--- tests/test_app.py
+++ tests/test_app.py
@@ -317,7 +317,7 @@ def test_verbose():
         main()
     with open('testdata/log18') as f:
         filecontent = f.read()
-        assert 'Clean_cut; ' in filecontent
+        assert 'CutModule: ' in filecontent
 
 
 def test_limit():
