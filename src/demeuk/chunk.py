ab79db25d20a1cf520e7446695cc87e302ec3c96
Melvin Frederiks <m.frederiks@nfi.nl>
2026-05-07 15:48:42 +0200

    Don't write to file early to prevent multiple processes writing to same file

--- src/demeuk/chunk.py
+++ src/demeuk/chunk.py
@@ -20,7 +20,6 @@ def check_finished_jobs(jobs, logger):
 
 def submit(pool, jobs, pipeline, chunk, config):
     while True:
-        check_finished_jobs(jobs, config.logger)
         running_jobs = sum([not job.ready() for job in jobs])
         if running_jobs < config.threads:
             jobs.append(pool.apply_async(pipeline.run, (chunk, config)))
